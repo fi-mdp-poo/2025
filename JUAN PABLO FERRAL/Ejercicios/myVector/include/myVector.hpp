fb8e7ed0a3c3d55a46f7db3ad805a879f697cca7
Ferral Juan Pablo <juanpitaferral@gmail>
2025-11-06 21:37:41 -0300

    Subo myVector terminado

--- JUAN PABLO FERRAL/Ejercicios/myVector/include/myVector.hpp
+++ JUAN PABLO FERRAL/Ejercicios/myVector/include/myVector.hpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <utility>
+#include <stdexcept>
 #include <cmath>
 
 using namespace std;
@@ -10,7 +11,6 @@ using namespace std;
 template <class T> class myVector
 
 {
-    //friend ostream& operator << (ostream&,const myVector&);
     //friend istream& operator >> (istream&,myVector&);
     public:
         myVector(size_t n=10);
@@ -21,23 +21,38 @@ template <class T> class myVector
         ~myVector();
         void push_back(const T&);
         T pop_back();
-        T get(int) const;
-        void set(int,const T&);
-        int getSize() const;
-        int getCapacity() const;
-        T operator [] (int) const;
-        T& operator [] (int);
-        void clear();
-        bool empty() const;
-        void redimensionP(int nuevaLongitud, const T& valorPorDefecto = T(0,0));
-        void insertar(int,const T&);
-        void eliminar(int);
+        T get(size_t) const;
+        void set(size_t, const T&);
+        int getSize() const { return longitud; }
+        int getCapacity() const { return capacidad; }
+        T operator [] (size_t) const;
+        T& operator [] (size_t);
+        void clear() { longitud = 0; }
+        bool empty() const { return (longitud == 0); }
+        void redimensionP(size_t nuevaLongitud, const T& dato);
+        void insertar(size_t, const T&);
+        void eliminar(size_t);
+
+        friend ostream& operator << (ostream& oS,const myVector<T>& obj)
+        {
+            oS << "[";
+            for(size_t i=0 ; i < obj.longitud ; i++)
+            {
+                oS << obj.arreglo[i];
+                if(i != (obj.longitud -1) )
+                {
+                    oS << ", ";
+                }
+            }
+            oS << "]";
+            return oS;
+        }
         
     private:
         size_t capacidad;
         size_t longitud;
         T* arreglo;
-        void redimension(size_t n);
+        void redimension(size_t n=0);
         void cambio(myVector<T>&) noexcept;
 };
 
@@ -120,7 +135,7 @@ template <class T> void myVector<T> :: redimension(size_t n)
     T* aux = new T[capacidad];
     try
     {
-        for(int i=0 ; i<longitud ; i++)
+        for(size_t i=0 ; i<longitud ; i++)
         {
             aux[i]=arreglo[i];
         }
@@ -130,12 +145,161 @@ template <class T> void myVector<T> :: redimension(size_t n)
     catch(...)
     {
         delete[] aux;
+        throw;
+    }
+}
+
+template <class T> void myVector<T> :: push_back(const T& n)
+{
+    if(longitud == capacidad)
+    {
+        redimension();
+    }
+    arreglo[longitud] = n;
+    longitud++;
+}
+
+template<class T> T myVector<T> :: pop_back()
+{
+    if(longitud == 0)
+    {
+        throw out_of_range("Llamada a pop_back con vector vacio");
+    }
+    else
+    {
+        longitud--;
+        return arreglo[longitud];
+    }
+}
+
+template<class T> T myVector<T> :: get(size_t index) const
+{
+    if(index >= longitud)
+    {
+        throw out_of_range("Indice fuera de rango en myVector::get()");
+    }
+    else
+    {
+        return arreglo[index];
     }
 }
 
 
+template<class T> void myVector<T> :: set(size_t index, const T& dato)
+{
+    if(index >= longitud)
+    {
+        throw out_of_range("Indice fuera de rango en myVector::set()");
+    }
+    else
+    {
+        arreglo[index] = dato;
+    }
+}
 
+template<class T> T myVector<T> ::operator [] (size_t index) const
+{
+    if(index >= longitud)
+    {
+        throw out_of_range("Indice fuera de rango en myVector::operator[]");
+    }
+    else
+    {
+        return arreglo[index];
+    }
+}
 
+template<class T> T& myVector<T> :: operator [] (size_t index)
+{
+    if(index >= longitud)
+    {
+        throw out_of_range("Indice fuera de rango en myVector::operator[]");
+    }
+    else
+    {
+        return arreglo[index];
+    }
+}
 
+template<class T> void myVector<T> :: redimensionP(size_t nuevaLongitud, const T& dato)
+{
+    if(nuevaLongitud < longitud)
+    {
+        longitud=nuevaLongitud;
+    }
+    else if(nuevaLongitud > longitud)
+    {
+        if(nuevaLongitud > capacidad)
+        {
+            redimension(nuevaLongitud);
+        }
+        for(size_t i = longitud ; i<nuevaLongitud ; i++)
+        {
+            arreglo[i]= dato;
+        }
+        longitud = nuevaLongitud;
+    }
+}
+
+template<class T> void myVector<T> :: insertar(size_t index, const T& dato)
+{
+    if(index >= longitud)
+    {
+        throw out_of_range("Indice fuera de rango en myVector::insertar()");
+    }
+    else
+    {
+        if(longitud == capacidad)
+        {
+            redimension();
+        }
+        for(size_t i=longitud ; i>index ; i--)
+        {
+            arreglo[i] = arreglo[i-1];
+        }
+        arreglo[index] = dato;
+        longitud++;
+    }
+}
+
+template<class T> void myVector<T> :: eliminar(size_t index)
+{
+    if(index >= longitud)
+    {
+        throw out_of_range("Indice fuera de rango en myVector::eliminar()");
+    }
+    else
+    {
+        --longitud;
+        for(size_t i=index ; i < longitud ; i++)
+        {
+            arreglo[i] = arreglo[i+1];
+        }
+    }
+}
+
+template<class T> ostream& operator << (ostream& oS,const myVector<T>& obj)
+{
+    oS << "[";
+    for(int i=0 ; i < obj.longitud ; i++)
+    {
+        oS << obj.arreglo[i];
+        if(i != (obj.longitud -1) )
+        {
+            oS << ", ";
+        }
+    }
+    oS << "]";
+    return oS;
+}
+
+/*istream& operator >> (istream& is,Cvector& v)
+{
+    Complejo numero;
+    cout << "Ingrese un Complejo(sera agregado al final del vector)" << endl;
+    is >> numero;
+    v.push_back(numero);
+    return is;
+}*/
 
 #endif // MYVECTOR_H
--- JUAN PABLO FERRAL/Ejercicios/myVector/src/main.cpp
+++ JUAN PABLO FERRAL/Ejercicios/myVector/src/main.cpp
@@ -1,11 +1,49 @@
 #include <iostream>
-#include "myVector.hpp"
-
 using namespace std;
 
-int main()
-{
-    myVector<int> p;
-    cout << "LISTO PADRE" ;
+
+#include "Complejo.hpp"
+#include "myVector.hpp"
+
+int main() {
+
+    // Creamos un Cvector de complejos
+    myVector<Complejo> v(3);
+
+    // Agregamos algunos complejos
+    v.push_back(Complejo(1, 2));
+    v.push_back(Complejo(3, 4));
+    v.push_back(Complejo(5, 6));
+    v.redimensionP(10, Complejo(0,0));
+
+    cout << "Vector inicial: " << v << endl;
+
+    // Probamos set en el segundo elemento
+    v.set(1, Complejo(9, 9));
+    cout << "Despues de set en index 1: " << v << endl;
+
+    // Probamos insertar en el medio
+    v.insertar(1, Complejo(7, 7));
+    cout << "Despues de insertar en index 1: " << v << endl;
+
+    // Probamos eliminar el ultimo
+    v.eliminar(v.getSize()-1);
+    cout << "Despues de eliminar el ultimo: " << v << endl;
+
+    // Probamos pop_back
+    Complejo c = v.pop_back();
+    cout << "Se saco con pop_back: " << c << endl;
+    cout << "Vector ahora: " << v << endl;
+
+    // Probamos empty y clear
+    cout << "Esta vacio? " << (v.empty() ? "si" : "no") << endl;
+    v.clear();
+    cout << "Despues de clear esta vacio? " << (v.empty() ? "si" : "no") << endl;
+    cout << "Vector ahora: " << v << endl;
+
+    int a;
+    cin >> a ;
+    a=0;
+
     return 0;
 }
